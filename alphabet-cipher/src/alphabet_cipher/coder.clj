012c2afafedc7146e4926bb0bf7b33d2fdc01829
kaz-yos <kazky@mac.com>
2015-01-03 20:49:20 -0500

    Add cipher skeletons

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,25 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [midje.sweet :as mj]))
 
+(mj/facts
+ (mj/fact
+  "Matrix conversion check"
+  (matrix (range 1 10))
+  => (partition-all 3 (range 1 10))))
+
+;;; Creation of the conversion table
+(def alphabets (map #(str (char %)) (range 97 (+ 97 26))))
+(def alphabets-infinite (cycle alphabets))
+
+(def conversion-seq-seq (for [n (range 0 26)]
+                          (take 26 (drop n alphabets-infinite))))
+
+
+
+;;; encoder
 (defn encode [keyword message]
   "encodeme")
 
+;;; decoder
 (defn decode [keyword message]
   "decodeme")
