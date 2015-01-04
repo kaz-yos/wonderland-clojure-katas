bedc51bfd36188900804492cb987c7c2fccc730a
kaz-yos <kazky@mac.com>
2015-01-03 20:52:40 -0500

    Add midje tests for conversion table configuration

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,19 +1,30 @@
 (ns alphabet-cipher.coder
   (:require [midje.sweet :as mj]))
 
-(mj/facts
- (mj/fact
-  "Matrix conversion check"
-  (matrix (range 1 10))
-  => (partition-all 3 (range 1 10))))
 
 ;;; Creation of the conversion table
+;; a-z sequence
 (def alphabets (map #(str (char %)) (range 97 (+ 97 26))))
+;; infinite lazy seq of a-z
 (def alphabets-infinite (cycle alphabets))
-
+;; 
 (def conversion-seq-seq (for [n (range 0 26)]
                           (take 26 (drop n alphabets-infinite))))
 
+(mj/facts
+ (mj/fact
+  "Check conversion table"
+  (class alphabets)
+  => clojure.lang.LazySeq
+
+  (class alphabets-infinite)
+  => clojure.lang.LazySeq
+
+  (count conversion-seq-seq)
+  => 26
+
+  (set (map count conversion-seq-seq))
+  => #{26}))
 
 
 ;;; encoder
