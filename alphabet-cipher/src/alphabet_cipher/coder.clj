e82c5ad6c0abf18665cb0dd41fed3495e66e821e
kaz-yos <kazky@mac.com>
2015-01-03 21:43:48 -0500

    Complete encode and decode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -16,7 +16,7 @@
        (map #(zipmap alphabets %), )
        (zipmap alphabets, )))
 
-
+;;; Test conversion tables
 (mj/facts
  (mj/fact
   "Check conversion table"
@@ -39,24 +39,103 @@
   (get-in conversion-map-map ["a" "a"])
   => "a"
   (get-in conversion-map-map ["b" "b"])
-  "c"
+  => "c"
+  (get-in conversion-map-map ["z" "z"])
+  => "y"
+  ))
+
+;;; Function to convert message to seq of str
+(defn seq-of-str
+  "Create a seq of strings from a string"
+  [word]
+  (->> word
+       (map char, )
+       (map str, )))
+
+;;; Function to create repeating keyword
+(defn keyword-repeated
+  "keyword repeated as long as message"
+  [keyword message]
+  (let [cycle-keyword (cycle keyword)]
+    (->> cycle-keyword
+         (take (count message), )
+         (map str, ))))
+
+(mj/facts
+ (mj/fact
+  "string conversion"
+  (seq-of-str "alphabets")
+  => '("a" "l" "p" "h" "a" "b" "e" "t" "s")
+
+  (keyword-repeated "abc" "12345678")
+  => '("a" "b" "c" "a" "b" "c" "a" "b")
+  ))
+
+;;; Encoder
+(defn encoder
+  "Encode a plain message and return a seq of strings"
+  [conversion-map-map keyword message]
+  (let [keyword-rep (keyword-repeated keyword message)]
+    (loop [key-str keyword-rep
+           mess-str (seq-of-str message)
+           acc []]
+      (if (empty? mess-str)
+        acc
+        (let [encoded-str (get-in conversion-map-map
+                                  [(first key-str) (first mess-str)])]
+          (recur (rest key-str) (rest mess-str) (conj acc encoded-str)))))))
+
+;;; Final function constructor
+(defn constructor
+  "Return a functional closure enclosing the conversion table"
+  [fun conversion-map-map]
+  (fn [keyword message]
+    (apply str (fun conversion-map-map keyword message))))
+
+;;; encoder
+(def encode
+  "Closure enclosing the conversion-map-map"
+  (constructor encoder conversion-map-map))
+
+(mj/facts
+ (mj/fact
+  "encoder test"
+  (encoder conversion-map-map "abc" "abc")
+  => '("a" "c" "e")
+
+  (encode "abc" "abc")
+  => "ace"
+
+  (encode "scones" "meetmebythetree")
+  => "egsgqwtahuiljgs"
   ))
 
 
-;;;
-(defn )
-
-
-
-;;; encoder
-(defn encode [keyword message]
-  "encodeme")
+;;; Decoder
+(defn decoder
+  "Decode an encrypted message and return a seq of strings"
+  [conversion-map-map keyword message]
+  (let [keyword-rep (keyword-repeated keyword message)]
+    (loop [key-str keyword-rep
+           mess-str (seq-of-str message)
+           acc []]
+      (if (empty? mess-str)
+        acc
+        ;; Select element in conversion-map-map by (first key-str)
+        ;; Then return the key of element matching (first mess-str)
+        (let [map-of-interest (conversion-map-map (first key-str))
+              inverted-map (clojure.set/map-invert map-of-interest)
+              decoded-str (inverted-map (first mess-str))]
+          (recur (rest key-str) (rest mess-str) (conj acc decoded-str)))))))
 
 ;;; decoder
-(defn decode [keyword message]
-  "decodeme")
+(def decode
+  "Closure enclosing the conversion-map-map"
+  (constructor decoder conversion-map-map))
 
 
-
-
-
+(mj/facts
+ (mj/fact
+  (decode "scones" "egsgqwtahuiljgs")
+  => "meetmebythetree"
+  ))
